943ffd11b03e9c55baea53c7b8dd359c98e83d56
Bijan Dayani <bdayani518@gmail.com>
2021-09-27 13:45:01 -0400

    js file rebuild

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,41 @@
 'use strict';
 
+// >>>>> DOM DISPLAY VARIABLES
+let directions = document.getElementById('game-directions');
+let word = document.getElementById('current-word');
+
 // >>>>> GAME OBJECT
 let game = {
-  // >>>>> words to play
-  angels: [
-    'adam',
-    'lilith',
-    'sachiel',
-    'shamshel',
-    'ramiel',
-    'gaghiel',
-    'israfel',
-    'sandalphon',
-    'matarael',
-    'sahaquiel',
-    'ireul',
-    'leliel',
-    'bardiel',
-    'zeruel',
-    'arael',
-    'armisael',
-    'tabris',
-    'lilin',
-  ],
-
-  // >>>>> CREATE ARRAY OF ALPHABET
-  alphabet: 'abcdefghijklmnopqrstuvwxyz'.split(''),
-  // console.log(alphabet[0]);
-
-  gameIstarted: false,
-
-  // >>>>> DOM DISPLAY VARIABLES
-  directions: document.getElementById('game-directions'),
-  word: document.getElementById('current-word'),
-
-  // >>>>> GUESSES MADE
-  guesses: 0,
-
-  // >>>>> GUESSES REMAINING
-  remainingGuesses: 10,
-  remainingLetters: this.alphabet,
-  // >>>>> GAME OBJECT METHODS
-  showWord: function () {
-    this.word.style.display = 'flex';
-    this.directions.style.display = 'none';
+  // array of characters
+  evaCharacters: {
+    shinji: { picture: './', song: '' },
+    rei: { picture: './', song: '' },
+    asuka: { picture: './', song: '' },
+    misato: { picture: './', song: '' },
+    ritsuko: { picture: './', song: '' },
   },
-  getAngel: function (array, current) {
-    current = array[0];
-    console.log(current);
-    this.word.textContent = current;
-    this.showWord();
+
+  // initial variables
+  currentCharacter: null,
+  nameArray: [],
+  userGuess: null,
+  matchingLetters: [],
+  guessedLetters: [],
+  attemptsMade: 0,
+  remainingAttempts: 0,
+  // initiliaze game
+
+  // write methods for game
+  startGame: function () {
+    let characterKeys = Object.keys(this.evaCharacters); // list of character names by plugging object keys into characterKeys variable
+
+    // console.log(characterKeys);
+    this.currentCharacter =
+      characterKeys[Math.floor(Math.random() * characterKeys.length)]; // select random word from characterKeys, store it into currentCharacter variable.
+    // console.log(this.currentCharacter);
+
+    this.nameArray = this.currentCharacter.split(''); // convert characterKeys to an array of letters
+    console.log(this.nameArray);
   },
 };
-// set word in play to array of characters
-
-// PSEUDOCODE GAME OBJECT METHODS:
-// begin game function
-// display currentAngel as underscore characters
-// capture user input (as lower case character, regardless of case)
-// show guessed letters as having been pressed
-// compare user guess to elements in currentAngel array
-
-game.getAngel(game.angels);
-// console.log(game.remainingLetters);
+game.startGame();

3235b5963a4e58a87e5fdd2a0940d0002ac7af8f
Bijan Dayani <bdayani518@gmail.com>
2021-09-27 16:33:39 -0400

    keyboard event listener, set blank letter values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,19 +23,51 @@ let game = {
   guessedLetters: [],
   attemptsMade: 0,
   remainingAttempts: 0,
-  // initiliaze game
 
-  // write methods for game
+  // >>>>> GAME METHODS
+  // RUN GAME
   startGame: function () {
-    let characterKeys = Object.keys(this.evaCharacters); // list of character names by plugging object keys into characterKeys variable
+    let characterNames = Object.keys(this.evaCharacters); // list of character names by plugging object key values into characterNames variable
 
-    // console.log(characterKeys);
+    // console.log(characterNames);
     this.currentCharacter =
-      characterKeys[Math.floor(Math.random() * characterKeys.length)]; // select random word from characterKeys, store it into currentCharacter variable.
+      characterNames[Math.floor(Math.random() * characterNames.length)]; // select random word from characterNames, store it into currentCharacter variable.
     // console.log(this.currentCharacter);
 
-    this.nameArray = this.currentCharacter.split(''); // convert characterKeys to an array of letters
+    this.nameArray = this.currentCharacter.split(''); // convert characterNames to an array of letters
     console.log(this.nameArray);
+    this.updateWordText(this.nameArray);
+  },
+
+  // UPDATE MATCHED LETTERS
+
+  // UPDATING DOM
+  updateWordText: function () {
+    let wordText = ''; // empty string value
+    // LOOP THROUGH ARRAY FOR GUESSED LETTERS
+    for (let i = 0; i < this.nameArray.length; i++) {
+      const element = this.nameArray[i];
+      if (this.matchingLetters.indexOf(element) !== -1) {
+        wordText += element;
+      } else {
+        wordText += ' _ ';
+      }
+    }
+    document.getElementById('current-word').textContent = wordText;
   },
 };
 game.startGame();
+
+// EVENT LISTENER FUNCTION
+document.onkeydown = function (e) {
+  // console.log(e.key);
+  if (e.key.length === 1 && !/[^a-zA-Z]/.test(e.key)) {
+    // alphabetic key validation using event.key
+    // console.log('yup');
+    game.userGuess = e.key.toUpperCase(); // assign game.userGuess to value of event.key
+    document.getElementById('current-word').style.display = 'flex';
+    document.getElementById('game-directions').style.display = 'none';
+  } else {
+    alert('Use keys a-z only!');
+  }
+};
